8582ad5be2258860e0a2617673a1ecff69d44a0a
Mahmud-Araf <almahmudaraf@gmail.com>
2023-08-11 20:02:35 +0600

    import optimized

--- backend/src/main/java/com/healthtechbd/backend/entity/AmbulanceProvider.java
+++ backend/src/main/java/com/healthtechbd/backend/entity/AmbulanceProvider.java
@@ -11,7 +11,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name="ambulance_prodiver")
+@Table(name="ambulance_provider")
 public class AmbulanceProvider {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
