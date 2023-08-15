892b1636eda2c0c6ad8ed66a38d6f5bb96220efa
Mahmud-Araf <almahmudaraf@gmail.com>
2023-08-15 11:50:39 +0600

    cross origin added

--- backend/src/main/java/com/healthtechbd/backend/controller/AuthController.java
+++ backend/src/main/java/com/healthtechbd/backend/controller/AuthController.java
@@ -19,13 +19,10 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
-
+@CrossOrigin(origins  = ("http://localhost:3000"))
 @RestController
 public class AuthController {
     @Autowired
